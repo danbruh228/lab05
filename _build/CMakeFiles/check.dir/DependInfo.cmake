02cde932b0f66689449153ae76f0398098f15b05
danbruh228 <vboxuser@tim.myguest.virtualbox.org>
2024-06-02 21:09:56 +0300

    init

---
+++ _build/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/WhiteSunOfSpace/workspace/lab05/lab05/tests/test_Account.cpp" "CMakeFiles/check.dir/tests/test_Account.cpp.o" "gcc" "CMakeFiles/check.dir/tests/test_Account.cpp.o.d"
+  "/home/vboxuser/WhiteSunOfSpace/workspace/lab05/lab05/tests/test_Transaction.cpp" "CMakeFiles/check.dir/tests/test_Transaction.cpp.o" "gcc" "CMakeFiles/check.dir/tests/test_Transaction.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vboxuser/WhiteSunOfSpace/workspace/lab05/lab05/_build/CMakeFiles/banking.dir/DependInfo.cmake"
+  "/home/vboxuser/WhiteSunOfSpace/workspace/lab05/lab05/_build/third-party/gtest/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/vboxuser/WhiteSunOfSpace/workspace/lab05/lab05/_build/third-party/gtest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
